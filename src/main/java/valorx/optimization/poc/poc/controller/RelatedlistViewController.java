5eac61de877c020092a21b8cfde559b03de2c226
Shubham Jaiswal <sjaiswal@valorx.com>
2024-04-04 18:37:49 +0530

    initial commit

---
+++ src/main/java/valorx/optimization/poc/poc/controller/RelatedlistViewController.java
@@ -0,0 +1,11 @@
+package valorx.optimization.poc.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/valorx")
+////public class RelatedlistViewController {
+////    @GetMapping("{related/view}")
+////}
